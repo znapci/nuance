2bb97c1d8b03c117498e48deb76cb08a4e47fc5e
numannaeem <numan.naeem9@gmail.com>
2021-12-11 21:26:16 +0530

    New fields

--- src/features/auth/login/RegisterModal.jsx
+++ src/features/auth/login/RegisterModal.jsx
@@ -10,16 +10,46 @@ import {
   Input,
   ModalFooter,
   ModalHeader,
-  Button
+  Button,
+  Grid,
+  GridItem,
+  Alert,
+  AlertIcon,
+  AlertTitle,
+  AlertDescription,
+  CloseButton,
+  Heading,
+  Text
 } from '@chakra-ui/react'
+import { backendUrl } from '../../../service/config'
 
 function RegisterModal ({ isOpen, onClose, finalRef }) {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
+  const [email, setEmail] = useState('')
+  const [age, setAge] = useState('')
+  const [realName, setRealName] = useState('')
+  const [success, setSuccess] = useState(false)
+  const [error, setError] = useState(null)
 
-  const registerUser = () => {
+  const registerUser = async () => {
     // do your fancy redux shit
-    onClose()
+    const res = await window.fetch(`${backendUrl}/api/signup`, {
+      method: 'POST',
+      body: {
+        username,
+        age,
+        email,
+        realName,
+        password
+      }
+    })
+    if (res.ok) {
+      setSuccess(true)
+    } else {
+      console.log(res)
+      setError(res.statusText)
+    }
   }
 
   const initialRef = useRef()
@@ -37,25 +67,86 @@ function RegisterModal ({ isOpen, onClose, finalRef }) {
         <ModalHeader>Create your account</ModalHeader>
         <ModalCloseButton />
         <ModalBody pb={6}>
-          <FormControl isRequired>
-            <FormLabel>Username</FormLabel>
-            <Input
-              value={username}
-              onChange={e => setUsername(e.target.value)}
-              ref={initialRef}
-              placeholder='Username'
-            />
-          </FormControl>
+          {success
+            ? (
+              <>
+                <Heading>Successfully registered!</Heading>
+                <Text>
+                  Check your inbox and verify your email to make the most of nuance!
+                </Text>
+              </>
+              )
+            : (
+              <>
+                {error && (
+                  <Alert status='error' mb={4}>
+                    <AlertIcon />
+                    <AlertTitle mr={2}>Something went wrong!</AlertTitle>
+                    <AlertDescription>{error}</AlertDescription>
+                    <CloseButton
+                      onClick={() => setError(false)}
+                      position='absolute'
+                      right='8px'
+                      top='8px'
+                    />
+                  </Alert>
+                )}
+                <FormControl isRequired>
+                  <FormLabel>Username</FormLabel>
+                  <Input
+                    value={username}
+                    onChange={e => setUsername(e.target.value)}
+                    ref={initialRef}
+                    placeholder='Username'
+                  />
+                </FormControl>
+                <FormControl isRequired mt={4}>
+                  <FormLabel>Password</FormLabel>
+                  <Input
+                    type='password'
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}
+                    placeholder='Password'
+                  />
+                </FormControl>
 
-          <FormControl isRequired mt={4}>
-            <FormLabel>Password</FormLabel>
-            <Input
-              type='password'
-              value={password}
-              onChange={e => setPassword(e.target.value)}
-              placeholder='Password'
-            />
-          </FormControl>
+                <FormControl isRequired mt={4}>
+                  <FormLabel>Email</FormLabel>
+                  <Input
+                    type='email'
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}
+                    ref={initialRef}
+                    placeholder='Email'
+                  />
+                </FormControl>
+                <Grid templateColumns='repeat(2, 1fr)' gap={4}>
+                  <GridItem>
+                    <FormControl isRequired mt={4}>
+                      <FormLabel>Real name</FormLabel>
+                      <Input
+                        value={realName}
+                        onChange={e => setRealName(e.target.value)}
+                        ref={initialRef}
+                        placeholder='Real Name'
+                      />
+                    </FormControl>
+                  </GridItem>
+                  <GridItem>
+                    <FormControl isRequired mt={4}>
+                      <FormLabel>Age</FormLabel>
+                      <Input
+                        type='number'
+                        value={age}
+                        onChange={e => setAge(e.target.value)}
+                        ref={initialRef}
+                        placeholder='Age'
+                      />
+                    </FormControl>
+                  </GridItem>
+                </Grid>
+              </>
+              )}
         </ModalBody>
 
         <ModalFooter>
